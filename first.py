99ce9b78d838f9ea3e585ba82f56de6d4efd2206
thadeus <thadeusnyariki94@gmail.com>
2023-05-18 02:12:44 +0300

    strings

---
+++ first.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+print("Hello, world!")
+
+#STRINGS
+#print string
+print("hello world")
+print('hello world')
+print(""" This string runs
+multiple lines """) #tripple quote for multi-line comment
+print("This string is "+"awesome") #we can also concatenate
+print('\n') #new line
+print('Test that new line out!')
