809824dc8a02eccd4a7b289ba973e33243099236
thadeus <thadeusnyariki94@gmail.com>
2023-05-18 02:24:23 +0300

    math

--- first.py
+++ first.py
@@ -11,3 +11,15 @@ multiple lines """) #tripple quote for multi-line comment
 print("This string is "+"awesome") #we can also concatenate
 print('\n') #new line
 print('Test that new line out!')
+
+print('\n')
+#MATH
+print(50 + 50) #add
+print(50 - 50) #substract
+print(50 * 50) #multiply
+print(50 / 50) #divide
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+print(50 ** 2) #exponents
+print(50 % 6) #modulo - takes whats left over
+print(50 / 6) #division with remainder (or float)
+print(50 // 6) #no remainder
