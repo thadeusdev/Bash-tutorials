81b48ec2b08132c1974631f564980facddc3e045
thadeus <thadeusnyariki94@gmail.com>
2023-05-18 03:05:03 +0300

    Functions

--- first.py
+++ first.py
@@ -49,3 +49,42 @@ print(age) #34
 birthday = 1
 age += birthday
 print(age) #35
+
+print('\n')
+
+#FUNCTIONS
+
+def who_am_i(): #this is a function without parameters
+    name = "Thadeus" #local variable
+    age = 30
+    print("my name is "+name+" and i am "+str(age)+" years old.")
+
+who_am_i()
+
+def add_one_hundred(num):
+    print(num + 100)
+
+add_one_hundred(100) #print out 200
+
+def add(x,y):
+    print(x + y)
+
+add(7,7) #print 14
+
+def multiply(x,y):
+    return x * y
+
+multiply(7,7) #prints nothing to screen
+print(multiply(7,7)) #49
+
+def square_root(x):
+    print(x ** .5)
+
+square_root(64) #8.0
+
+def nl():
+    print('\n')
+
+nl()
+
+#BOOLEAN EXPRESSIONS AND RELATIONAL OPERATORS
